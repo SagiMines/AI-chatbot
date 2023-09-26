7b227c17e4ad4ac9ea7e0612500cb17f5d9074ab
Sagi Mines <sagi1236@gmail.com>
2023-09-26 17:27:34 +0300

    Added chat-form component

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 'use client';
 import Chat from '@/components/Chat';
+import ChatForm from '@/components/ChatForm';
+import axios from 'axios';
 import { useState } from 'react';
 
 export default function Home() {
@@ -9,18 +11,58 @@ export default function Home() {
   );
   const [isLoading, setIsLoading] = useState(false);
 
-  const handleSubmit = (event: Event) => {
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     setChatLog(prevChatLog => [
       ...prevChatLog,
       { type: 'user', message: inputValue },
     ]);
+    await sendMessage(inputValue);
     setInputValue('');
   };
 
+  const sendMessage = async (message: string) => {
+    const url = 'https://api.pawan.krd/v1/chat/completions';
+    const headers = {
+      Authorization: `Bearer ${process.env.NEXT_PUBLIC_OPENAI_API_KEY}`,
+      'Content-type': 'application/json',
+    };
+    const data = {
+      model: 'pai-001-light-beta',
+      messages: [
+        {
+          role: 'user',
+          content: message,
+        },
+      ],
+    };
+
+    setIsLoading(true);
+
+    try {
+      const response = await axios.post(url, data, { headers });
+      console.log(response);
+      setChatLog(prevChatLog => [
+        ...prevChatLog,
+        { type: 'bot', message: response.data.choices[0].message.content },
+      ]);
+      setIsLoading(false);
+    } catch (error) {
+      setIsLoading(false);
+      console.log(error);
+    }
+  };
+
   return (
-    <main>
-      <Chat chatLog={chatLog} />
+    <main className="bg-gray-900 w-full h-full">
+      <div className="flex flex-col h-screen mx-auto max-w-[900px]">
+        <Chat chatLog={chatLog} />
+        <ChatForm
+          inputValue={inputValue}
+          setInputValue={setInputValue}
+          handleSubmit={handleSubmit}
+        />
+      </div>
     </main>
   );
 }
--- components/Chat.tsx
+++ components/Chat.tsx
@@ -8,10 +8,29 @@ type Props = {
 const Chat = ({ chatLog }: Props) => {
   return (
     <>
-      <h1>AI Chatbot</h1>
-      {chatLog.map((message, index) => (
-        <div key={index}>{message.message}</div>
-      ))}
+      <h1 className="bg-gradient-to-r from-blue-500 to-purple-500 text-transparent bg-clip-text text-center py-3 font-bold text-6xl">
+        AI Chatbot
+      </h1>
+      <div className="flex-grow p-6">
+        <div className="flex flex-col space-y-4">
+          {chatLog.map((message, index) => (
+            <div
+              className={`flex ${
+                message.type === 'user' ? 'justify-end' : 'justify-start'
+              }`}
+              key={index}
+            >
+              <div
+                className={`${
+                  message.type === 'user' ? 'bg-purple-500' : 'bg-gray-800'
+                } rounded-lg p-4 text-white max-w-sm`}
+              >
+                {message.message}
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
     </>
   );
 };

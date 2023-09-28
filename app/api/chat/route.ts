c85c840a647e9729426ea96331297e613cdbbc7b
Sagi Mines <sagi1236@gmail.com>
2023-09-28 15:12:53 +0300

    Added documention to the project's functions

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,12 +1,16 @@
 import { NextRequest, NextResponse } from 'next/server';
 import axios from 'axios';
 
+/* 
+API Call to 'https://api.pawan.krd/v1' ChatGPT alternative 
+AI api to get the answers.
+*/
 export async function POST(request: NextRequest) {
   try {
     const body = await request.json();
     const url = 'https://api.pawan.krd/v1/chat/completions';
     const headers = {
-      Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
+      Authorization: `Bearer ${process.env.CHATBOT_API_KEY}`,
       'Content-type': 'application/json',
     };
     const ApiResponse = await axios.post(url, body, { headers });

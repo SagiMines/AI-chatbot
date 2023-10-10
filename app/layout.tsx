33c2ad1ca3f232abbc826a2c57df27beed3b5300
Sagi Mines <sagi1236@gmail.com>
2023-10-10 15:49:14 +0300

    Updated metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,8 +4,10 @@ import type { Metadata } from 'next';
 export const metadata: Metadata = {
   title: 'AI Chatbot',
   description: 'Your Personal Chatbot Assistant',
-  viewport:
-    'width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no, target-densityDpi=device-dpi',
+  viewport: {
+    width: 'device-width',
+    initialScale: 1,
+  },
 };
 
 export default function RootLayout({
